b260354347d2f3417758b25c92cbd8b56e34129f
Ajith Gopinath <ajithgopinath639@gmail.com>
2024-04-26 18:43:18 +0530

    fix all errors in button

--- crudp/home/urls.py
+++ crudp/home/urls.py
@@ -4,6 +4,6 @@ from.import views
 urlpatterns = [
     path('',views.index,name="index"),
     path('insertData',views.insertData,name="insertData"),
-    path('updateData/<id>',views.updateData,name="updateData"),
-    path('deleteData/<id>',views.deleteData,name="deleteData"),
+    path('updatedata/<id>',views.updateData,name="updatedata"),
+    path('deletedata/<id>',views.deleteData,name="deletedata"),
 ]
--- crudp/home/views.py
+++ crudp/home/views.py
@@ -22,10 +22,10 @@ def insertData(request):
 
 def updateData(request,id):
      if request.method =="POST":
-        name=request.POST.get['name']
-        email=request.POST.get['email']
-        age=request.POST.get['age']
-        gender=request.POST.get['gender']
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        age=request.POST.get('age')
+        gender=request.POST.get('gender')
         edit=student.objects.get(id=id)
         edit.name=name
         edit.email=email
